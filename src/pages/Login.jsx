4ad5177673532bbd9e6bec2dad153211424bffc9
dntart <dantecesarsegovia@gmail.com>
2025-08-02 21:57:48 -0800

    contenido estatico login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,9 +3,29 @@ import { Layout } from "../components/Layout";
 const Login = () => {
     return (
         <>
-            <Layout background="blue">
-                <h2>Hola desde el login</h2>
-            </Layout>
+             <Layout>
+      <h1>Inicia sesión</h1>
+
+      <section>
+        <h2>Hola, bienvenido de nuevo</h2>
+        
+        <form>
+          <div>
+            <label>Correo electronico:</label>
+            <input
+              type="text"
+              name="email" />
+          </div>
+          <div>
+            <label>Contraseña:</label>
+            <input
+              type="password"
+              name="password" />
+          </div>
+          <button>Ingresar</button>
+        </form>
+      </section>
+    </Layout>
         </>
     )
 }
