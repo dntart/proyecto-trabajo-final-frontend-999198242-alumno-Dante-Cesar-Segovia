4800dcffec5a06a551bc3480f2c2091ec3626c44
dntart <dantecesarsegovia@gmail.com>
2025-08-07 18:34:32 -0800

    funcionalidad en botones delete en home y agregar en dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,37 +1,84 @@
 import { Layout } from "../components/Layout";
+import { useState } from "react";
 
 const Dashboard = () => {
-    return (
-        <>
-            <Layout>
-                <h1>Panel de Administración</h1>
+    const [name, setName] = useState()
+    const [price, setPrice] = useState()
+    const [description, setDescription] = useState()
 
-                <section>
-                    <h2>Cargar nuevo producto</h2>
-                    <form >
-                        <div>
-                            <label>Nombre del producto:</label>
-                            <input type="text" name="nombre" />
-                        </div>
+    const handleSubmit = async (e) => {      // para agregar producto
+        e.preventDefault()
 
-                        <div>
-                            <label>Precio:</label>
-                            <input type="number" name="precio" />
-                        </div>
+        const newProduct = {
+            id: crypto.randomUUID(),   // metodo mas usado para dar un id que que no se repita
+            title: name,               // la API espera una propiedad title 
+            price: parseFloat(price),  // convertir a numero
+            description: description,
+            category: "una categoria de ej.",
+            image: "imagen pendiente"
+        }
+        console.log(newProduct)
 
-                        <div>
-                            <label>Descripción:</label>
-                            <textarea name="descripcion" rows="4" />
-                        </div>
+        try {
+            const response = await fetch('https://fakestoreapi.com/products', { // este formato viene en la API *(en la web)
+                method: 'POST',                                       // metodo de envio ENVIAR POST
+                headers: { 'Content-Type': 'application/json' },    // le decimos a la APY que va un json 
+                body: JSON.stringify(newProduct)                       // Convertimos el js a json
+            })
+            const data = await response.json()
+            console.log("producto enviado con exito", data)
+            alert("producto enviado con exito")
+            console.log(data)
 
-                        <button>Guardar producto</button>
-                    </form>
+            setName("")  // para limpiar inputs
+            setPrice("")
+            setDescription("")
+        }
+        catch (error) {
 
-                    
-                </section>
-            </Layout>
-        </>
-    )
-}
+            console.error("error al enviar el produco", error)
+            alert("error al enviar el produco")
+        }
+
+
+
+    }
+
+
+return (
+    <>
+        <Layout>
+            <h1>Panel de Administración</h1>
+
+            <section>
+                <h2>Cargar nuevo producto</h2>
+                <form onSubmit={handleSubmit} >
+                    <div>
+                        <label>Nombre del producto:</label>
+                        <input type="text" name="nombre"
+                            onChange={(e) => setName(e.target.value)} />
+                    </div>
+
+                    <div>
+                        <label>Precio:</label>
+                        <input type="number" name="precio"
+                            onChange={(e) => setPrice(e.target.value)} />
+                    </div>
+
+                    <div>
+                        <label>Descripción:</label>
+                        <textarea name="descripcion" rows="4"
+                            onChange={(e) => setDescription(e.target.value)} />
+                    </div>
+
+                    <button>Guardar producto</button>
+                </form>
+
+
+            </section>
+        </Layout>
+    </>
+)
+    }
 
 export { Dashboard }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,70 +14,83 @@ const Home = () => {
         console.log(data)
     }
 
-    // useEffect sirve para ejecutar una funcion en un momento especifico
+    // useEffect sirve para ejecutar una funcion en un momento especifico // UI  
     useEffect(() => {
         fetchingProducts()
     }, []) // el array vacio [] es la dependencia a.puede estar vacio, b.con una funcion, c. sin array se renderiza siempre
 
     const handleDelete = async (id) => {
-        const response = await fetch(`https://fakestoreapi.com/test/${id}`, 
-            {method: "DELETE"})
-console.log (response)
+        const response = await fetch(`https://fakestoreapi.com/products/${id}`,
+            { method: "DELETE" })
+        console.log(response)
 
- 
+        if (response.ok) {
+            setProducts(prevProducts => prevProducts.filter((product) => product.id != id)) // actualizar estado a partir de estado anterior representado por prevProducts =>
         }
-    return (
-        <>
-            <Layout background="red">
-                <section>
-                    <h1>Bienvenido a Nuestra Tienda</h1>
-                    <p>Descubrí una selección exclusiva de productos para vos. Calidad, confianza y atención personalizada.</p>
-                </section>
+    }
 
-                <section>
-                    <h2>¿Por qué elegirnos?</h2>
-                    <ul>
-                        <li>
-                            <h3>Envíos a todo el país</h3>
-                            <p>Recibí tu compra en la puerta de tu casa estés donde estés.</p>
-                        </li>
-                        <li>
-                            <h3>Pagos seguros</h3>
-                            <p>Trabajamos con plataformas que garantizan tu seguridad.</p>
-                        </li>
-                        <li>
-                            <h3>Atención personalizada</h3>
-                            <p>Estamos disponibles para ayudarte en todo momento.</p>
-                        </li>
-                    </ul>
-                </section>
+    //Peticion al backend para modificar -> metodo PATCH
+    const handleUpdate = async (id) => {
+        const response = await fetch(`https://fakestoreapi.com/products/${id}`, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(handleUpdate)
+        })
 
-                <section>
-                    <h2>Nuestros productos</h2>
-                    <p>Elegí entre nuestras categorías más populares.</p>
-                </section>
-                <div>
-                    {products.map((product) =>
-                        <div key={product.id}>
-                            <h2> {product.title} </h2>
-                            <img style={{ width: "100px" }} src={product.image} alt={`imagen de ${product.title}`} />
-                            <p>$ {product.price}</p>
-                            <p>{product.description}</p>
-                            <p><strong>{product.category}</strong></p>
-                            {user &&             // los botones solo se muestran si hay usuario logueado (true), en false los botones no deben aparecer
-                                <div>
-                                    <button>Actualizar</button>
-                                    <button onClick={() => handleDelete(product.id)}>Borrar</button>
-                                </div>}
-                        </div>
+        }
+
+        return (
+            <>
+                <Layout background="red">
+                    <section>
+                        <h1>Bienvenido a Nuestra Tienda</h1>
+                        <p>Descubrí una selección exclusiva de productos para vos. Calidad, confianza y atención personalizada.</p>
+                    </section>
+
+                    <section>
+                        <h2>¿Por qué elegirnos?</h2>
+                        <ul>
+                            <li>
+                                <h3>Envíos a todo el país</h3>
+                                <p>Recibí tu compra en la puerta de tu casa estés donde estés.</p>
+                            </li>
+                            <li>
+                                <h3>Pagos seguros</h3>
+                                <p>Trabajamos con plataformas que garantizan tu seguridad.</p>
+                            </li>
+                            <li>
+                                <h3>Atención personalizada</h3>
+                                <p>Estamos disponibles para ayudarte en todo momento.</p>
+                            </li>
+                        </ul>
+                    </section>
+
+                    <section>
+                        <h2>Nuestros productos</h2>
+                        <p>Elegí entre nuestras categorías más populares.</p>
+                    </section>
+                    <div>
+                        {products.map((product) =>
+                            <div key={product.id}>
+                                <h2> {product.title} </h2>
+                                <img style={{ width: "100px" }} src={product.image} alt={`imagen de ${product.title}`} />
+                                <p>$ {product.price}</p>
+                                <p>{product.description}</p>
+                                <p><strong>{product.category}</strong></p>
+                                {user &&             // los botones solo se muestran si hay usuario logueado (true), en false los botones no deben aparecer
+                                    <div>
+                                        <button onClick={() => handleUpdate(product.id)}>Actualizar</button>
+                                        <button onClick={() => handleDelete(product.id)}>Borrar</button>
+                                    </div>}
+                            </div>
 
 
-                    )}
-                </div>
+                        )}
+                    </div>
 
-            </Layout>
+                </Layout>
 
-        </>
-    )
-}
-export { Home }
+            </>
+        )
+    }
+    export { Home }
