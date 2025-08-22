c3e37056007117ac713e95889e9219daac0b0482
dntart <dantecesarsegovia@gmail.com>
2025-08-21 22:05:56 -0800

    estilado de footer y correcion de contacto

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,12 +16,12 @@ const Footer = () => {
                     <li><Link to={"about"}>Sobre Nosotros</Link></li>
                     <li><Link to={"login"}>Iniciar Sesión</Link></li>
                     <li><Link to={"register"}>Registrarse</Link></li>
-                    
+
                 </ul>
             </div>
             <div className={styles.contacto}>
-                <h3>Desarrollado por <a href="https://ar.linkedin.com/in/dante-cesar-segovia-583046258?original_referer=https%3A%2F%2Fwww.google.com%2F" target="_blank">Dante César Segovia</a>
-                </h3>
+                <h3>Contacto                </h3>
+                <p>Desarrollado por <a href="https://ar.linkedin.com/in/dante-cesar-segovia-583046258?original_referer=https%3A%2F%2Fwww.google.com%2F" target="_blank">Dante César Segovia</a></p>
                 <p>© 2024 Mi Tienda. Todos los derechos reservados.</p>
             </div>
         </footer>
